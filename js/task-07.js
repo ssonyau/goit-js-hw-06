916265787a21a93a51ad5ad44564c2513fdd7a16
Sonya Tkachenko <94766215+ssonyau@users.noreply.github.com>
2023-02-13 19:17:49 +0200

    /

--- js/task-07.js
+++
@@ -1,6 +0,0 @@
-const slider = document.querySelector('#font-size-control');
-const targetSpan = document.querySelector('#text');
-targetSpan.style.fontSize = `${slider.value}px`;
-slider.addEventListener('input', () => {
-    targetSpan.style.fontSize = `${slider.value}px`;
-})
